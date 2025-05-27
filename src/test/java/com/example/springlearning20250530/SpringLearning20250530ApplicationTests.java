f28de9147b22de51d35e7052f82440b7e8226436
MJ <yuemjmahu@gmail.com>
2025-05-27 23:27:33 +0900

    Initial Spring boot project setup

---
+++ src/test/java/com/example/springlearning20250530/SpringLearning20250530ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlearning20250530;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearning20250530ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
