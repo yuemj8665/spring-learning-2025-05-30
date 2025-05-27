077ae44a059ec7c6cdea313cb638f8c84b9b6647
MJ <yuemjmahu@gmail.com>
2025-05-27 23:42:29 +0900

    2025-05-27 REST API TEST1

--- src/main/java/com/example/springlearning20250530/HelloworldContoller.java
+++ src/main/java/com/example/springlearning20250530/HelloworldContoller.java
@@ -1,10 +1,12 @@
 package com.example.springlearning20250530;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloworldContoller {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot! You are using Spring Boot!";
     }
 }
--- src/main/java/com/example/springlearning20250530/SpringLearning20250530Application.java
+++ src/main/java/com/example/springlearning20250530/SpringLearning20250530Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLearning20250530Application {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringLearning20250530Application.class, args);
     }
-
 }
